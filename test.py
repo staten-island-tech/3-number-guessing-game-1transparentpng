35108111040d3752affc6588ccd8960265101801
1transparentpng <aidens151@nycstudents.net>
2025-09-30 08:53:04 -0400

    Complete (also 67)

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import math
+import random
 
 guessHistory = []
 guess = 0
 
-genNumber = 
+genNumber = random.randint(0,100) # generates a random integer using the random library
 
 while guess is not genNumber:
-    guess = input("Try to guess the generated number!!")
-    if guess < genNumber:
+    guess = int(input("Try to guess the generated number!!")) # converts the input to integer
+    if guess == genNumber:
+        print(f"Congrats, You got it right! The generated number was {genNumber}.")
+        print(f"Past Guesses: {guessHistory}")
+    elif guess < genNumber:
         print("WRONG!!!!!!! Too low of a number!")
         print(f"Past Guesses: {guessHistory}")
         guessHistory.append(guess)
@@ -15,3 +18,4 @@ while guess is not genNumber:
         print("WRONG!!!!!!! Too high of a number!")
         print(f"Past Guesses: {guessHistory}")
         guessHistory.append(guess)
+    
